a4ae27551b953d76f45c538e18805f3cfc0a51f5
Nadia Manasterska <118932184+nadiamanasterska@users.noreply.github.com>
2024-11-04 23:02:48 +0100

    Update data_generation.py

--- data_generation.py
+++ data_generation.py
@@ -1,34 +1,17 @@
-# data_generation.py
-
 import pandas as pd
 from sklearn.datasets import make_blobs
-from sklearn.datasets import load_digits
-from sklearn.decomposition import PCA
 
 
 def generate_data():
-    X, _ = make_blobs(
+    # Generate synthetic data with labels
+    X, y = make_blobs(
         n_samples=300,
         centers=4,
         cluster_std=2.3,
-        random_state=42
+        random_state=43
     )
+    # Create a DataFrame with features and labels
     df = pd.DataFrame(X, columns=['Feature 1', 'Feature 2'])
+    df['Label'] = y  # Add the true labels to the DataFrame
     return df
 
-'''
-
-def generate_data():
-    
-    digits = load_digits()
-    X = digits.data  
-
-    # pca reduction
-    pca = PCA(n_components=2)
-    X_pca = pca.fit_transform(X)
-
-    df = pd.DataFrame(X_pca, columns=['Feature 1', 'Feature 2'])
-
-    return df
-
-'''
