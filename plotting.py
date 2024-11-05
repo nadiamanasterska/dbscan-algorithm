4ed3a03aa01d2731e2553e35b96558f858f974da
Nadia Manasterska <118932184+nadiamanasterska@users.noreply.github.com>
2024-11-05 16:58:53 +0100

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,19 +1,20 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 
-def plot_data(df, noise_points=None, cluster_column_custom='Cluster_custom', cluster_column_kmeans='Cluster_kmeans'):
+def plot_data(df, noise_points=None, cluster_column_custom='Cluster_custom', cluster_column_kmeans='Cluster_kmeans', cluster_column_dbscan_sklearn='Cluster_dbscan_sklearn', cluster_column_hdbscan='Cluster_hdbscan'):
     # Ustawienia wykresu
-    fig, axes = plt.subplots(1, 3, figsize=(18, 6))
+    fig, axes = plt.subplots(1, 5, figsize=(18, 6))
     
-    # Wykres lewy - dane wygenerowane z etykietami
+    # Wykres pierwszy - dane wygenerowane z etykietami
     sns.scatterplot(
         x='Feature 1', y='Feature 2', hue='Label', data=df, 
         palette='viridis', s=60, ax=axes[0], legend=False
     )
     axes[0].set_title("True Labels (Generated Data)")
     
-    # Wykres środkowy - wyniki klasteryzacji własnej implementacji DBSCAN
+    # Wykres drugi - wyniki klasteryzacji własnej implementacji DBSCAN
     sns.scatterplot(
         x='Feature 1', y='Feature 2', hue=cluster_column_custom, data=df, 
         palette='tab10', s=60, ax=axes[1], legend=False
@@ -26,14 +27,30 @@ def plot_data(df, noise_points=None, cluster_column_custom='Cluster_custom', clu
         )
     axes[1].set_title("Clusters (Custom DBSCAN Result)")
 
-    # Wykres prawy - wyniki klasteryzacji K-Means
+    # Wykres trzeci - wyniki klasteryzacji K-Means
     sns.scatterplot(
-        x='Feature 1', y='Feature 2', hue=cluster_column_kmeans, data=df, 
+        x='Feature 1', y='Feature 2', hue=cluster_column_kmeans, data=df,
         palette='tab10', s=60, ax=axes[2], legend=False
     )
     axes[2].set_title("Clusters (K-Means Result)")
     axes[2].set_xlabel("Feature 1")
     axes[2].set_ylabel("Feature 2")
+
+    # Wykres czwarty - wyniki DBSCAN z scikit-learn
+    sns.scatterplot(
+        x='Feature 1', y='Feature 2', hue=cluster_column_dbscan_sklearn, data=df,
+        palette='tab10', s=60, ax=axes[3], legend=False
+    )
+    axes[3].set_title("Clusters (scikit-learn DBSCAN Result)")
+    axes[3].set_xlabel("Feature 1")
+    axes[3].set_ylabel("Feature 2")
+
+    # Wykres piąty - wyniki klasteryzacji HDBSCAN
+    sns.scatterplot(
+        x='Feature 1', y='Feature 2', hue=cluster_column_hdbscan, data=df,
+        palette='tab10', s=60, ax=axes[4], legend=False
+    )
+    axes[4].set_title("Clusters (HDBSCAN Result)")
 
     plt.tight_layout()
     plt.show()
