6d000c724e6d173f47a446655723591580dc0a14
Nadia Manasterska <118932184+nadiamanasterska@users.noreply.github.com>
2024-11-05 14:52:25 +0100

    Update data_generation.py

--- data_generation.py
+++ data_generation.py
@@ -1,17 +1,32 @@
 import pandas as pd
-from sklearn.datasets import make_blobs
+from sklearn.datasets import make_blobs, load_digits
+from sklearn.decomposition import PCA
 
 
 def generate_data():
-    # Generate synthetic data with labels
+
+
+
+    # generating using make blobs
     X, y = make_blobs(
         n_samples=300,
         centers=4,
         cluster_std=2.3,
         random_state=43
     )
-    # Create a DataFrame with features and labels
-    df = pd.DataFrame(X, columns=['Feature 1', 'Feature 2'])
-    df['Label'] = y  # Add the true labels to the DataFrame
-    return df
 
+
+    ''' 
+    X, y = make_blobs(
+        n_samples=300,
+        centers=3,
+        cluster_std=2.3,
+        random_state=26
+    )
+    '''
+
+
+    # Tworzenie DataFrame z funkcjami i etykietami
+    df = pd.DataFrame(X, columns=['Feature 1', 'Feature 2'])
+    df['Label'] = y  # Dodanie prawdziwych etykiet do DataFrame
+    return df
